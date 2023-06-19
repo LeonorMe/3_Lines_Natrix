faad42fe8e9e60f9efb340f187a8e3d70892b784
LeonorMe <leonomedeiros@gmail.com>
2023-06-19 19:37:09 +0100

    sound files addded, polyline save works (path to poly)

--- src/shapes/Polyline.java
+++ src/shapes/Polyline.java
@@ -2,11 +2,13 @@ package shapes;
 
 import org.w3c.dom.Element;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.awt.*;
 import java.util.ArrayList;
 
 public class Polyline extends AbstShape {
-    // <polyline points="20,20 40,25 60,40 80,120 120,140 200,180" style="fill:none;stroke:black;stroke-width:3" />
+    // Exemple SVG <polyline points="20,20 40,25 60,40 80,120 120,140 200,180" style="fill:none;stroke:black;stroke-width:3" />
     ArrayList<Ponto> pontos;
     public Polyline(String id, int x1, int y1, ArrayList<Ponto> pontos, String style) {
         super.AbstShape(id, x1, y1, style);
@@ -16,7 +18,7 @@ public class Polyline extends AbstShape {
     // GET
     @Override
     public String getType(){
-        return "path";
+        return "polyline";
     }
     public ArrayList<Ponto> getPontos(){
         return this.pontos;
@@ -76,15 +78,6 @@ public class Polyline extends AbstShape {
         s += super.toString();
         return s;
     }
-    /*
-    @Override
-    public boolean equals(Object o){
-        if (o == this) return true;
-        if (!(o instanceof Polyline)) return false;
-        Polyline p = (Polyline) o;
-        return super.equals(p) && this.pontos.equals(p.getPontos());
-    }
-     */
 
     @Override
     public void drawShape(Graphics g) {
@@ -94,13 +87,44 @@ public class Polyline extends AbstShape {
     }
 
     public boolean isInside(int currentX, int currentY, int size) {
-        // TODO
         if(!super.isInside(currentX, currentY)){
-            for (Ponto ponto: pontos) {
-                if(currentX - ponto.getX() < size && currentY - ponto.getY() < size)
+            Line line;
+            for (int i= 0; i < this.getPontos().size() - 1; i++) {
+                line = new Line(Integer.toString(i), this.getPonto(i).getX(), this.getPonto(i).getY(), this.getPonto(i+1).getX(), this.getPonto(i+1).getY(), this.getStyle());
+                if(line.isInside(currentX, currentY)){
                     return true;
+                }
             }
         }
         return false;
     }
+    /*
+    public void move(int currentX, int currentY, int size) {
+        int x = currentX - this.getX1();
+        int y = currentY - this.getY1();
+        this.setX1(currentX);
+        this.setY1(currentY);
+        for (int i = 0; i < this.getPontos().size(); i++) {
+            this.getPonto(i).setX(this.getPonto(i).getX() + x);
+            this.getPonto(i).setY(this.getPonto(i).getY() + y);
+        }
+    }*/
+
+    /*
+    public static void main(String[] args) throws ParserConfigurationException, TransformerException {
+        ArrayList<Ponto> pontos = new ArrayList<>();
+        pontos.add(new Ponto(20, 20));
+        pontos.add(new Ponto(40, 25));
+        pontos.add(new Ponto(60, 40));
+        pontos.add(new Ponto(80, 120));
+        pontos.add(new Ponto(120, 140));
+        pontos.add(new Ponto(200, 180));
+        Polyline polyline = new Polyline("poly_1", 20, 20, pontos, "fill:none;stroke:black;stroke-width:3");
+        polyline.show();
+
+        ImageSVG svg = new ImageSVG(500, 500);
+        svg.addShape(polyline);
+        svg.saveSVG("polyline.svg");
+    }
+     */
 }
